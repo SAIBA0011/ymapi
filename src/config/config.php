bb9624374b1d067f2c9f36cd665f558048895939
Gerhard <gerhardtheunissen10@gmail.com>
2014-11-17 14:41:00 +0200

    Updated Config file

--- src/config/config.php
+++ src/config/config.php
@@ -11,9 +11,12 @@ return [
     // Your Yourmembership SA Pass Code
     'sapass' => '**********',
 
+    // Your Default Yourmembership username
     'username' => 'John',
 
+    // Your Default Yourmembership password
     'password' => 'password',
 
+    // Current API Version
     'version' => '2.00'
 ];

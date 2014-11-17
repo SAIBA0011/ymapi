e7f383d1cdaed97bf76f9ef6fe935d80109b994d
Gerhard <gerhardtheunissen10@gmail.com>
2014-11-17 13:30:04 +0200

    update

--- src/config/config.php
+++ src/config/config.php
@@ -3,14 +3,14 @@
 return [
 
     // Your Yourmembership Public API Key
-    'pubkey' => 'B6F18415-9A4B-4D54-87CF-32842E0F4039',
+    'pubkey' => '',
 
     // Your Yourmembership Private API Key
-    'prikey' => 'DF8EA27A-FB42-4D13-AC38-87A4F0707E7E',
+    'prikey' => '',
 
     // Your Yourmembership SA Pass Code
-    'sapass' => 'TP8Jb5YNi8k0',
+    'sapass' => '',
 
     // Your Yourmembership API Version ( stick to 2.00 )
-    'version' => '2.00'
+    'version' => ''
 ];

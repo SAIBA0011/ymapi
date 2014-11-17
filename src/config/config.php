f9099b61ca301a1ec72a1fb7e0188a4a8745179f
Gerhard <gerhardtheunissen10@gmail.com>
2014-11-17 17:55:45 +0200

    Updated Config

--- src/config/config.php
+++ src/config/config.php
@@ -3,19 +3,19 @@
 return [
 
     // Your Yourmembership Public API Key
-    'pubkey' => 'B6F18415-9A4B-4D54-87CF-32842E0F4039',
+    'pubkey' => '************************',
 
     // Your Yourmembership Private API Key
-    'prikey' => 'DF8EA27A-FB42-4D13-AC38-87A4F0707E7E',
+    'prikey' => '************************',
 
     // Your Yourmembership SA Pass Code
-    'sapass' => 'TP8Jb5YNi8k0',
+    'sapass' => '************************',
 
     // Your Default Yourmembership username
-    'username' => 'gerhardt0011',
+    'username' => '************************',
 
     // Your Default Yourmembership password
-    'password' => 'Sparxz986532',
+    'password' => '************************',
 
     // Current API Version
     'version' => '2.00'
